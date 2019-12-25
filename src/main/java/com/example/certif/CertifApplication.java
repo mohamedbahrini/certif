8c9797b4ec05bb5b7ee8c72ecf02ada3c3e5d141
Mohamed Bahrini <mohamed.bahrini@hotmail.com>
2019-12-25 16:43:01 +0100

    initial commit

---
+++ src/main/java/com/example/certif/CertifApplication.java
@@ -0,0 +1,13 @@
+package com.example.certif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/certif/CertifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
